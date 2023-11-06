8e29be43bac6909bfdd6dc383c4eb09b7d699c87
jb6131 <44068903+jb6131@users.noreply.github.com>
2023-11-06 17:28:51 -0600

    added project links

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,6 +2,7 @@ export default function Resume() {
   return (
     <div className="container mt-3">
       <h1 className="text-center">Resume</h1>
+      <p className="text-center fs-4 mt-5">Coming soon!</p>
     </div>
   );
 }
